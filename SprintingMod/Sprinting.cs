6b1d6f5afb291e18fdda61ff42030f4728e0b640
oliverpl <patrick.l.oliver@gmail.com>
2016-03-26 16:06:08 -0700

    Updated to use buffs and SMAPI 0.39.1

--- SprintingMod/Sprinting.cs
+++ SprintingMod/Sprinting.cs
@@ -1,90 +1,157 @@
 ﻿using StardewModdingAPI;
+using StardewModdingAPI.Inheritance;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
+using System;
 
 namespace SprintingMod
 {
     public class Sprinting : Mod
     {
-        public override string Name
-        {
-            get { return "Sprinting Mod"; }
-        }
-
-        public override string Authour
-        {
-            get { return "Patrick Oliver"; }
-        }
-
-        public override string Version
-        {
-            get { return "1.0"; }
-        }
-
-        public override string Description
-        {
-            get { return "Allows the user to increase speed when pressing a button."; }
-        }
-
-        private static int sprintingSpeed = 3;
-        private static int currentSprintingSpeed = 0;
-        private static string sprintingButton = "17";
+        public static SprintingModConfig Config { get; set; }
         private const string _debuggerInfo = "[SprintingMod INFO] ";
+        private Buff SprintingBuff { get; set; }
+        private int timeSinceLastDrain = 0;
 
         public override void Entry(params object[] objects)
         {
-            Log.Info(_debuggerInfo + "Getting settings from SprintingMod.ini file.");
-            UpdateSettings();
-            Log.Info(_debuggerInfo + "Initializing KeyboardInput listeners.");
+            Config = new SprintingModConfig().InitializeConfig(BaseConfigPath);
+            Log.Info(_debuggerInfo + "Initializing buff with drain and rate: " + Config.StaminaDrain + "/" + Config.StaminaDrainRate);
+            BuffInit();
             KeyboardInput.KeyDown += KeyboardInput_KeyDown;
             KeyboardInput.KeyUp += KeyboardInput_KeyUp;
-
-            GameEvents.GameLoaded += GameEvents_GameLoaded;
+            ControlEvents.ControllerButtonPressed += ControllerButtonPressed;
+            ControlEvents.ControllerButtonReleased += ControllerButtonReleased;
+            GameEvents.OneSecondTick += GameEvents_OneSecondTick;
             
         }
 
-        private void GameEvents_GameLoaded(object sender, System.EventArgs e)
+        private void BuffInit()
         {
-            currentSprintingSpeed = Game1.player.speed;
+            SprintingBuff = new Buff(0, 0, 0, 0, 0, 0, 0, 0, 0, Config.SprintSpeed, 0, 0, 1200, "Sprint Mod");
+            SprintingBuff.which = Buff.speed;
+            SprintingBuff.sheetIndex = Buff.speed;
         }
 
-        private void UpdateSettings()
+        private void GameEvents_OneSecondTick(object sender, EventArgs e)
         {
-            var parser = new IniFileReader();
-            var settings = parser.GetSettings("SprintingMod.ini");
-            if(settings != null && settings.Count > 1)
+            if (SGame.player.isMoving() && SprintingBuff_Exists())
             {
-                Log.Info(_debuggerInfo + "Applying settings from ini file.");
-                sprintingSpeed = settings["SprintSpeed"].AsInt32();
-                sprintingButton = settings["KeyToPress"];
+                timeSinceLastDrain += 1;
+                if (timeSinceLastDrain >= Config.StaminaDrainRate)
+                {
+                    SGame.player.Stamina -= Config.StaminaDrain;
+                    timeSinceLastDrain = 0;
+                }
+            }
+            else
+            {
+                timeSinceLastDrain = 0;
             }
         }
 
         private void KeyboardInput_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode.ToString() == sprintingButton)
+            if (e.KeyCode.ToString().Equals(Config.SprintKey))
             {
-                player_sprint();
+                if (Config.HoldToSprint)
+                    Player_Sprint();
+                else
+                    Player_Toggle_Sprint();
             }
         }
 
         private void KeyboardInput_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode.ToString() == sprintingButton)
+            if (e.KeyCode.ToString().Equals(Config.SprintKey))
             {
-                player_walk();
+                if (Config.HoldToSprint)
+                {
+                    Player_Walk();
+                }
             }
         }
 
-        static void player_sprint()
+        private void ControllerButtonPressed(object sender, EventArgsControllerButtonPressed e)
         {
-            Game1.player.addedSpeed = sprintingSpeed;
+            if (e.ButtonPressed.ToString().Equals(Config.SprintKey))
+            {
+                if (Config.HoldToSprint)
+                    Player_Sprint();
+                else
+                    Player_Toggle_Sprint();
+            }
+        }
+
+        private void ControllerButtonReleased(object sender, EventArgsControllerButtonReleased e)
+        {
+            if (e.ButtonReleased.ToString().Equals(Config.SprintKey))
+            {
+                if (Config.HoldToSprint)
+                {
+                    Player_Walk();
+                }
+            }
+        }
+
+        private void Player_Sprint()
+        {
+            if (!SprintingBuff_Exists())
+                SGame.buffsDisplay.addOtherBuff(SprintingBuff);
         }
         
-        static void player_walk()
+        private void Player_Walk()
         {
-            Game1.player.addedSpeed = currentSprintingSpeed;
+            if (SprintingBuff_Exists())
+            {
+                SGame.buffsDisplay.otherBuffs.Remove(SprintingBuff);
+                SprintingBuff.removeBuff();
+                SGame.buffsDisplay.syncIcons();
+            }
+        }
+
+        private void Player_Toggle_Sprint()
+        {
+            if (SprintingBuff_Exists())
+            {
+                SGame.buffsDisplay.otherBuffs.Remove(SprintingBuff);
+                SprintingBuff.removeBuff();
+                SGame.buffsDisplay.syncIcons();
+            }
+            else
+            {
+                SGame.buffsDisplay.addOtherBuff(SprintingBuff);
+            }
+        }
+
+        private bool SprintingBuff_Exists()
+        {
+            if (SprintingBuff == null)
+                return false;
+
+            return SGame.buffsDisplay.otherBuffs.Contains(SprintingBuff);
+        }
+    }
+
+    public class SprintingModConfig : Config
+    {
+        public bool HoldToSprint { get; set; }
+        public int SprintSpeed { get; set; }
+        public string SprintKey { get; set; }
+        public int StaminaDrain { get; set; }
+        public int StaminaDrainRate { get; set; }
+        public string SprintKeyForControllers { get; set; }
+
+        public override T GenerateDefaultConfig<T>()
+        {
+            HoldToSprint = true;
+            SprintSpeed = 3;
+            SprintKey = "17";
+            SprintKeyForControllers = "LeftStick";
+            StaminaDrain = 1;
+            StaminaDrainRate = 5;
+            return this as T;
         }
     }
 }
